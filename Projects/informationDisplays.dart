d826aabf90681b0cb2d1fbfa74ac52e023ead0d4
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-02-02 13:45:53 +0300

    Update informationDisplays.dart

--- Projects/informationDisplays.dart
+++ Projects/informationDisplays.dart
@@ -1,4 +1,3 @@
-// loading and other display widgets
 import 'package:flutter/material.dart';
 
 void main() => runApp(const DisplayApp());
@@ -38,6 +37,191 @@ class _DisplayAppState extends State<DisplayApp> {
             const SizedBox(width: 20),
           ],
         ),
+        body: Center(
+          child: GridView.count(
+            padding: const EdgeInsets.all(10),
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+            crossAxisCount: 3,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: isDark ? Colors.amber : Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: CircularPIApp(),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: isDark ? Colors.amber : Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: CIPwDetermined(),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: isDark ? Colors.amber : Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: isDark ? Colors.amber : Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CircularPIApp extends StatefulWidget {
+  const CircularPIApp({super.key});
+  @override
+  State<CircularPIApp> createState() => _CircularPIAppState();
+}
+
+class _CircularPIAppState extends State<CircularPIApp>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Circular progress indicator with a fixed color',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          CircularProgressIndicator(
+            strokeWidth: 5,
+            value: controller.value,
+            valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+            semanticsLabel: "Circular progress indicator",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CIPwDetermined extends StatefulWidget {
+  const CIPwDetermined({super.key});
+  @override
+  State<CIPwDetermined> createState() => _CIPwDeterminedState();
+}
+
+class _CIPwDeterminedState extends State<CIPwDetermined>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+  bool determinate = false;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'CircularProgressIndicator width Determined ("Switch()")',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 15),
+          CircularProgressIndicator(
+            strokeWidth: 5,
+            value: controller.value,
+            semanticsLabel: "CircularProgressIndıcator",
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  'Determinate Mode (play / pause)',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+              Switch(
+                  value: determinate,
+                  activeColor: Colors.greenAccent,
+                  onChanged: (bool value) {
+                    setState(() {
+                      determinate = value;
+                      if (determinate) {
+                        controller.stop();
+                      } else {
+                        controller
+                          ..forward(from: controller.value)
+                          ..repeat();
+                      }
+                    });
+                  }),
+            ],
+          ),
+        ],
       ),
     );
   }
