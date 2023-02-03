fbc3a733632c37a836a3c446dda9b876d4aed005
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-02-03 10:34:18 +0300

    Create simpleTable.dart

---
+++ Table/simpleTable.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const TableApp());
+
+class TableApp extends StatefulWidget {
+  const TableApp({super.key});
+  @override
+  State<TableApp> createState() => _TableAppState();
+}
+
+class _TableAppState extends State<TableApp> {
+  bool isDark = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: isDark ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Basic Table App")),
+          actions: <Widget>[
+            Center(
+              child: Text(
+                isDark ? "LIGHT" : "DARK",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: isDark ? Colors.amberAccent : Colors.black),
+              ),
+            ),
+            const SizedBox(width: 15),
+            Switch(
+                value: isDark,
+                activeColor: Colors.amber,
+                onChanged: (bool value) {
+                  setState(() {
+                    isDark = !isDark;
+                  });
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
