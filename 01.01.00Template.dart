9e293d399c708e01c91cb52798b1e8eb5b2a1bc2
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-02-03 10:36:55 +0300

    Create 01.01.00Template.dart

---
+++ 01.01.00Template.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const TemplateApp());
+
+class TemplateApp extends StatefulWidget {
+  const TemplateApp({super.key});
+  @override
+  State<TemplateApp> createState() => _TemplateAppState();
+}
+
+class _TemplateAppState extends State<TemplateApp> {
+  bool isDark = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: isDark ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Basic Template App")),
+          actions: <Widget>[
+            Center(
+              child: Text(
+                isDark ? "LIGHT" : "DARK",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: isDark ? Colors.amberAccent : Colors.black),
+              ),
+            ),
+            const SizedBox(width: 15),
+            Switch(
+                value: isDark,
+                activeColor: Colors.amber,
+                onChanged: (bool value) {
+                  setState(() {
+                    isDark = !isDark;
+                  });
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
