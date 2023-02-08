a1e5314d4c1e09474ccdf8c2fe4e54aa60e338cc
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-02-08 10:58:15 +0300

    Update AnimtedAlign.dart

--- Animations/AnimtedAlign.dart
+++ Animations/AnimtedAlign.dart
@@ -53,7 +53,9 @@ class AnimationAlign extends StatefulWidget {
 }
 
 class _AnimationAlignState extends State<AnimationAlign> {
-  bool selected = false;
+  bool selected1 = false;
+  bool selected2 = false;
+  bool selected3 = false;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -62,39 +64,122 @@ class _AnimationAlignState extends State<AnimationAlign> {
         crossAxisSpacing: 5,
         crossAxisCount: 2,
         children: <Widget>[
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                selected = !selected;
-              });
-            },
-            child: Center(
-              child: Container(
-                width: 250,
-                height: 250,
-                color: Colors.red,
-                child: AnimatedAlign(
-                  alignment:
-                      selected ? Alignment.topRight : Alignment.bottomLeft,
-                  duration: const Duration(seconds: 2),
-                  curve: Curves.fastOutSlowIn,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("Curves.fastOutSlowIn"),
+              const SizedBox(height: 15),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selected1 = !selected1;
+                  });
+                },
+                child: Center(
                   child: Container(
-                    width: 50,
-                    height: 50,
-                    margin: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.teal,
-                      border: Border.all(width: 1),
-                      borderRadius: BorderRadius.circular(50),
+                    width: 250,
+                    height: 250,
+                    color: Colors.red,
+                    child: AnimatedAlign(
+                      alignment:
+                          selected1 ? Alignment.topRight : Alignment.bottomLeft,
+                      duration: const Duration(seconds: 2),
+                      curve: Curves.fastOutSlowIn,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        margin: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.teal,
+                          border: Border.all(width: 1),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("Curves.easeInOut"),
+              const SizedBox(height: 15),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selected2 = !selected2;
+                  });
+                },
+                child: Center(
+                  child: Container(
+                    width: 250,
+                    height: 250,
+                    color: Colors.green,
+                    child: AnimatedAlign(
+                      alignment: selected2
+                          ? Alignment.centerLeft
+                          : Alignment.centerRight,
+                      duration: const Duration(seconds: 2),
+                      curve: Curves.easeInOut,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        margin: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          border: Border.all(width: 1),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text("Curves.easeOutCubic"),
+              const SizedBox(height: 15),
+              GestureDetector(
+                onTap: () => setState(() => selected3 = !selected3),
+                child: Center(
+                  child: Container(
+                    width: 250,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.pinkAccent,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: AnimatedAlign(
+                      alignment: selected3
+                          ? Alignment.topCenter
+                          : Alignment.bottomCenter,
+                      duration: const Duration(seconds: 2),
+                      curve: Curves.easeOutCubic,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: selected3 ? Colors.lime : Colors.indigo,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: selected3
+                            ? const Icon(Icons.arrow_circle_down_outlined,
+                                size: 30)
+                            : const Icon(Icons.arrow_upward_outlined, size: 30),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
 }
-
