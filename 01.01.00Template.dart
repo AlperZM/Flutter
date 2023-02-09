54f651f8e3c660e74368bbdedd839db0605f5e44
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-02-09 13:58:53 +0300

    Update 01.01.00Template.dart

--- 01.01.00Template.dart
+++ 01.01.00Template.dart
@@ -1,65 +1,28 @@
+// a basic template for flutter learning projects
+
 import 'package:flutter/material.dart';
-
 void main() => runApp(const TemplateApp());
-
 class TemplateApp extends StatefulWidget {
   const TemplateApp({super.key});
-  @override
-  State<TemplateApp> createState() => _TemplateAppState();
-}
-
+  @override State<TemplateApp> createState() => _TemplateAppState();}
 class _TemplateAppState extends State<TemplateApp> {
-  bool isDark = false;
-  @override
+  bool isDark = false;  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: isDark ? ThemeData.dark() : ThemeData.light(),
-      home: Scaffold(
-        appBar: AppBar(
+      home: Scaffold(appBar: AppBar(
           title: const Center(child: Text("Basic Template App")),
           actions: <Widget>[
-            Center(
-              child: Text(
-                isDark ? "LIGHT" : "DARK",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: isDark ? Colors.amberAccent : Colors.black),
-              ),
-            ),
+            Center( child: Text( isDark ? "LIGHT" : "DARK", style: TextStyle( fontSize: 20, 
+             color: isDark ? Colors.amberAccent : Colors.black),),),
             const SizedBox(width: 15),
-            Switch(
-                value: isDark,
-                activeColor: Colors.amber,
-                onChanged: (bool value) {
-                  setState(() {
-                    isDark = !isDark;
-                  });
-                }),
-          ],
-        ),
-        
-        body:const Center(
-        
-        child: TemplateClass()),
-        
-      ),
-    );
-  }
-}
-
+            Switch(value: isDark,activeColor: Colors.amber, onChanged: (bool value) {
+                  setState(() {isDark = !isDark;});}),],),
+        body:const Center(child: TemplateClass()),),);}}
 class TemplateClass extends StatefulWidget{
   const TemplateClass({super.key});
-  @override
-  State<TemplateClass> createState() => _TemplateClassState();
-}
-
+  @override  State<TemplateClass> createState() => _TemplateClassState();}
 class _TemplateClassState extends State<TemplateClass>{
-  
-  @override
-  Widget build(BuildContext context){
-    return Center(
-    
-    );
-  }
-}
+    @override  Widget build(BuildContext context){
+    return Center();}}
