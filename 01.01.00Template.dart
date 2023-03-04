e689034dd4fe2f9f54132c8d08b0fe76b94c5408
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-03-04 14:41:09 +0300

    Update 01.01.00Template.dart

--- 01.01.00Template.dart
+++ 01.01.00Template.dart
@@ -1,9 +1,7 @@
 // a basic template for flutter learning projects
 //https://zapp.run/
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';void main() => runApp(const TemplateApp());class TemplateApp extends StatefulWidget {
-const TemplateApp({super.key}); @override
-State<TemplateApp> createState() => _TemplateAppState();}
+import 'package:flutter/material.dart';void main() => runApp(const TemplateApp());class TemplateApp extends StatefulWidget {
+const TemplateApp({super.key}); @override State<TemplateApp> createState() => _TemplateAppState();}
 class _TemplateAppState extends State<TemplateApp>{bool isDark = false;@override
 Widget build(BuildContext context){return MaterialApp(debugShowCheckedModeBanner: false,
 theme: isDark ? ThemeData.dark() : ThemeData.light(),home: Scaffold(appBar: AppBar(
@@ -12,8 +10,7 @@ Center(child: Text(isDark ? "LIGHT" : "DARK",style: TextStyle(fontSize: 20,
 color: isDark ? Colors.amberAccent : Colors.black),),),const SizedBox(width: 15),
 Switch(value: isDark,activeColor: Colors.amber,onChanged: (bool value){
 setState((){isDark = !isDark;});}),],),body: const Center(child: TemplateClass()),
-),);}}
-class TemplateClass extends StatefulWidget{ const TemplateClass({super.key});
+),);}}class TemplateClass extends StatefulWidget{ const TemplateClass({super.key});
 @override State<TemplateClass> createState() => _TemplateClassState();}
 class _TemplateClassState extends State<TemplateClass> {@override
-Widget build(BuildContext context){return Center();}}
+Widget build(BuildContext context){return const Center();}}
