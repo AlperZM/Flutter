bbe11c3a275ebe001e099593c32931d40506d8f4
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-05-23 13:49:40 +0300

    Update Align.dart
    Predefined Alignment properties

--- 002 Widget List/Align.dart
+++ 002 Widget List/Align.dart
@@ -2,121 +2,256 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const AlignApp());
 
-class AlignApp extends StatelessWidget{
+class AlignApp extends StatelessWidget {
   const AlignApp({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-    debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-      appBar: AppBar(title: const Center(child: Text("Align App"))),
-      body: const Center(child: AlignDemos()),
+        appBar: AppBar(title: const Center(child: Text("Align App"))),
+        body: const Center(child: AlignDemos()),
       ),
     );
   }
 }
 
-class AlignDemos extends StatelessWidget{
+class AlignDemos extends StatelessWidget {
   const AlignDemos({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return GridView.count(
-  padding: const EdgeInsets.all(20),
-  crossAxisSpacing: 10,
-  mainAxisSpacing: 10,
-  crossAxisCount: 2,
-  children: <Widget>[
-    Container(
-      width: 120, height: 120,
-      decoration: BoxDecoration(
-      border: Border.all(width: 1),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            child:const Align(
-          alignment: Alignment.center,
-            child: FlutterLogo(size: 50),
-          )),
-          const Text("Center"),
-        ],
-      ),
-    ),
-    Container(
-      width: 120, height: 120,
-      decoration: BoxDecoration(
-      border: Border.all(width: 1),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            child:const Align(
-          alignment: Alignment.center,
-            child: FlutterLogo(size: 50),
-          )),
-          const Text("Center"),
-        ],
-      ),
-    ),
-     Container(
-      width: 120, height: 120,
-      decoration: BoxDecoration(
-      border: Border.all(width: 1),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            child:const Align(
-          alignment: Alignment.center,
-            child: FlutterLogo(size: 50),
-          )),
-          const Text("Center"),
-        ],
-      ),
-    ),
-     Container(
-      width: 120, height: 120,
-      decoration: BoxDecoration(
-      border: Border.all(width: 1),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-
-        children: <Widget>[
-          Container(
-            child:const Align(
-          alignment: Alignment.center,
-            child: FlutterLogo(size: 50),
-          )),
-          const Text("Center"),
-        ],
-      ),
-    ),
-     Container(
-      width: 120, height: 120,
-      decoration: BoxDecoration(
-      border: Border.all(width: 1),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            child:const Align(
-          alignment: Alignment.center,
-            child: FlutterLogo(size: 50),
-          )),
-          const Text("Center"),
-        ],
-      ),
-    ),
-  ],
-);
+      padding: const EdgeInsets.all(10),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 3,
+      children: <Widget>[
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 160,
+                alignment: Alignment.center,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text("alignment: Alignment.center",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 150,
+                alignment: Alignment.bottomCenter,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                child: const Text("alignment: Alignment.bottomCenter",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 150,
+                alignment: Alignment.bottomLeft,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                child: const Text("alignment: Alignment.bottomLeft",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 150,
+                alignment: Alignment.bottomRight,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                child: const Text("alignment: Alignment..bottomRight",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 150,
+                alignment: Alignment.centerLeft,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                child: const Text("alignment: Alignment.centerLeft",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 150,
+                alignment: Alignment.centerRight,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                child: const Text("alignment: Alignment.centerRight",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 150,
+                alignment: Alignment.topCenter,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                child: const Text("alignment: Alignment.topCenter",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 160,
+                alignment: Alignment.topLeft,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text("alignment: Alignment.topLeft",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.teal[100],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 160,
+                alignment: Alignment.topRight,
+                child: const FlutterLogo(size: 50),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text("alignment: AlignmenttopRight",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
