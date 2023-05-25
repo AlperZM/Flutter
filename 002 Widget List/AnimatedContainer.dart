37640ce9ee4df09536f051bf1aa665b2ce71ed13
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-05-25 15:55:52 +0300

    Update AnimatedContainer.dart

--- 002 Widget List/AnimatedContainer.dart
+++ 002 Widget List/AnimatedContainer.dart
@@ -1,52 +1,43 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [AnimatedContainer].
+void main() => runApp(const AnimatedContainerApp());
 
-void main() => runApp(const AnimatedContainerExampleApp());
+class AnimatedContainerApp extends StatefulWidget {
+  const AnimatedContainerApp({super.key});
+  @override
+  State<AnimatedContainerApp> createState() => _AnimatedContainerAppState();
+}
 
-class AnimatedContainerExampleApp extends StatelessWidget {
-  const AnimatedContainerExampleApp({super.key});
-
+class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
+  bool selected = false;
+  int time = 2;
+  Color colors = Colors.red;
+  double wi = 200;
+  int hei = 100;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('AnimatedContainer Sample')),
-        body: const AnimatedContainerExample(),
-      ),
-    );
-  }
-}
-
-class AnimatedContainerExample extends StatefulWidget {
-  const AnimatedContainerExample({super.key});
-
-  @override
-  State<AnimatedContainerExample> createState() =>
-      _AnimatedContainerExampleState();
-}
-
-class _AnimatedContainerExampleState extends State<AnimatedContainerExample> {
-  bool selected = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selected = !selected;
-        });
-      },
-      child: Center(
-        child: AnimatedContainer(
-          width: selected ? 200.0 : 100.0,
-          height: selected ? 100.0 : 200.0,
-          color: selected ? Colors.red : Colors.blue,
-          alignment:
-              selected ? Alignment.center : AlignmentDirectional.topCenter,
-          duration: const Duration(seconds: 2),
-          curve: Curves.fastOutSlowIn,
-          child: const FlutterLogo(size: 75),
+        appBar:
+            AppBar(title: const Center(child: Text("AnimatedContainer App"))),
+        body: GestureDetector(
+          onTap: () {
+            setState(() {
+              selected = !selected;
+            });
+          },
+          child: Center(
+            child: AnimatedContainer(
+              width: selected ? 200.0 : 100.0,
+              height: selected ? 100.0 : 200.0,
+              color: selected ? Colors.red : Colors.blue,
+              alignment:
+                  selected ? Alignment.center : AlignmentDirectional.topCenter,
+              duration: const Duration(seconds: 2),
+              curve: Curves.fastOutSlowIn,
+            ),
+          ),
         ),
       ),
     );
