71e67af344b8062fffeb74b3ca29e25e8ed8d0b5
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-05-29 15:08:07 +0300

    Update Badge.dart

--- 002 Widget List/Badge.dart
+++ 002 Widget List/Badge.dart
@@ -1,1 +1,70 @@
 
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BadgeApp());
+
+class BadgeApp extends StatelessWidget {
+  const BadgeApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Center(child: Text("BadgeApp"))),
+        body: const Center(child: BadgeDemo()),
+      ),
+    );
+  }
+}
+
+class BadgeDemo extends StatelessWidget {
+  const BadgeDemo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Badge.count(
+          // set the text style
+          textStyle: const TextStyle(
+            fontSize: 22,
+          ),
+          // set the Badge style
+          largeSize: 32,
+          alignment: const FractionalOffset(2, 0.5),
+          count: 5,
+          child: const Icon(Icons.favorite_border, size: 50, color: Colors.red),
+        ),
+        const SizedBox(width: 20),
+        Badge.count(
+          // set the text style
+          textStyle: const TextStyle(
+            fontSize: 22,
+          ),
+          // set the Badge style
+          largeSize: 32,
+          alignment: const FractionalOffset(2, 0.5),
+          count: 5,
+          child: const Icon(Icons.warning, size: 50, color: Colors.red),
+        ),
+        const SizedBox(width: 20),
+        Badge.count(
+          // set the text style
+          textStyle: const TextStyle(
+            fontSize: 22,
+          ),
+          backgroundColor: Colors.green,
+          textColor: Colors.black,
+          // set the Badge style
+          largeSize: 32,
+          alignment: const FractionalOffset(1.5, 0.5),
+          count: 5,
+          child: TextButton(
+            child: const Text("TextButton"),
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
+  }
+}
