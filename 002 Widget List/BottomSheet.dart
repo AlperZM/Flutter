2ba3f4e718610eb008569d6008c6f3bdfa3329af
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-05-30 12:23:11 +0300

    Create BottomSheet.dart

---
+++ 002 Widget List/BottomSheet.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BottomSheetApp());
+
+class BottomSheetApp extends StatelessWidget {
+  const BottomSheetApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Center(child: Text('BottomSheetApp'))),
+        body: const BottomSheetDemo(),
+      ),
+    );
+  }
+}
+
+class BottomSheetDemo extends StatelessWidget {
+  const BottomSheetDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            child: const Text("Persistent Bottom Sheet"),
+            onPressed: () {
+              Scaffold.of(context).showBottomSheet<void>(
+                (BuildContext context) {
+                  return Container(
+                    height: 300,
+                    color: Colors.amber,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text("""PersistenBottomSheet: 
+                          To Close is press "back arrow" on the appbar or "close button"
+                          Comes with LocalHistoryEntry and ModalRoute
+                          Can add many widgets.
+                          while this open, you can use the rest of app
+                          """, style: TextStyle(fontSize: 16)),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            child: const Text("Print Console"),
+                            onPressed: () {
+                              print("Printed");
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                              child: const Text("Alert Dialog"),
+                              onPressed: () {
+                                showDialog<String>(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text("Alert title"),
+                                        content: const Text("Alert Content"),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child: const Text("Ok"),
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    });
+                              }),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            child: const Text("Close the Bottom Sheet"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
