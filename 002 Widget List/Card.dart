0a750379588ff1e450d90ebae01288d8811015af
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-05-31 16:33:19 +0300

    Update Card.dart
    Add details and cards

--- 002 Widget List/Card.dart
+++ 002 Widget List/Card.dart
@@ -22,29 +22,92 @@ class CardDemos extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        Card(
-        child: Column(
-        mainAxisSize: MainAxisSize.min,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-          const  ListTile(
-            title: Text("Card Title"),
-            subtitle: Text("Card Subtitle and some descriptions"),
-            ),
-            Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextButton(
-                child: const Text("Cancel"),
-                  onPressed: (){},
+            Container(
+              width: 220,
+              height: 220,
+              margin: const EdgeInsets.all(10),
+              child: Card(
+                elevation: 8,
+                shadowColor: Colors.amber,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.shop),
+                      title: Text("Card Title"),
+                      subtitle: Text("Card Subtitle and some descriptions"),
+                    ),
+                    Container(
+                      width: 200,
+                      height: 100,
+                      color: Colors.amber,
+                      child: const Center(child: Text("Product Img")),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        ElevatedButton(
+                          child: const Text("Add To Chart"),
+                          onPressed: () {},
+                        ),
+                        ElevatedButton(
+                          child: const Text("Details"),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                
-              ],
+              ),
+            ),
+            Container(
+              width: 220,
+              height: 220,
+              margin: const EdgeInsets.all(10),
+              child: Card(
+                elevation: 8,
+                shadowColor: Colors.teal,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.restaurant),
+                      title: Text("Restaurant"),
+                      subtitle: Text("Order meal for dinner "),
+                    ),
+                    Container(
+                      width: 200,
+                      height: 100,
+                      color: Colors.teal,
+                      child: const Center(child: Text("Product Img")),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Badge.count(
+                          count: 15,
+                          child: const Icon(Icons.favorite,
+                              size: 32, color: Colors.red),
+                        ),
+                        TextButton(
+                          child: const Text("Details"),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
-        ),
-      ],     
+      ],
     );
   }
 }
