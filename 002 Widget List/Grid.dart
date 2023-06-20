1a411d271fdecfbeee29ec5219396276baa95ca0
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-06-20 12:47:29 +0300

    Create Grid.dart

---
+++ 002 Widget List/Grid.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const GridApp());
+
+class GridApp extends StatelessWidget {
+  const GridApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Center(child: Text("GridApp"))),
+        body: const Center(child: GridDemo()),
+      ),
+    );
+  }
+}
+
+class GridDemo extends StatelessWidget {
+  const GridDemo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(10),
+              width: 210,
+              height: 210,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: GridView.count(
+                padding: const EdgeInsets.all(5),
+                mainAxisSpacing: 2,
+                crossAxisSpacing: 2,
+                crossAxisCount: 4,
+                children: <Widget>[
+                  Container(
+                    color: Colors.teal[100],
+                    child: const Text("Default"),
+                  ),
+                  Container(
+                    color: Colors.teal[200],
+                    child: const Text("Grid"),
+                  ),
+                  Container(
+                    color: Colors.teal[300],
+                    child: const Text("View"),
+                  ),
+                  Container(
+                    color: Colors.teal[400],
+                    child: const Text("Default"),
+                  ),
+                  Container(
+                    color: Colors.amber[400],
+                  ),
+                  Container(
+                    color: Colors.amber[300],
+                  ),
+                  Container(
+                    color: Colors.amber[200],
+                  ),
+                  Container(
+                    color: Colors.amber[100],
+                  ),
+                  Container(
+                    color: Colors.green,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    color: Colors.red,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                  ),
+                  Container(
+                    color: Colors.cyan,
+                  ),
+                  Container(
+                    color: Colors.brown,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                  ),
+                  Container(
+                    color: Colors.cyan,
+                  ),
+                  Container(
+                    color: Colors.brown,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
