651575f4f0ae799ad66b0babb7425c6861994a4d
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-06-23 13:10:49 +0300

    Update 003_samples.dart

--- 002 Widget List/003_samples.dart
+++ 002 Widget List/003_samples.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const DemosApp());
 
+//-------->  parent class for MaterialApp
 class DemosApp extends StatelessWidget {
   const DemosApp({super.key});
   @override
@@ -22,9 +23,11 @@ class DemosBody extends StatefulWidget {
   State<DemosBody> createState() => _DemosBodyState();
 }
 
+//---------> for body Satefulwidget
 class _DemosBodyState extends State<DemosBody> {
   @override
   Widget build(BuildContext context) {
+//---------> GridView, every cell has diffrnt widget
     return GridView.count(
       crossAxisCount: 2,
       padding: const EdgeInsets.all(5),
@@ -39,7 +42,44 @@ class _DemosBodyState extends State<DemosBody> {
           child: const Text("This is a collection of some widgets.",
               textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
         ),
-        Container(width: 100, height: 100, color: Colors.blue[100]),
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.blue[100],
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: <Widget>[
+              const Text("AbsorbPointer"),
+              SizedBox(
+                width: 100,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    print("Pressed");
+                  },
+                  child: null,
+                ),
+              ),
+              SizedBox(
+                width: 50,
+                height: 100,
+
+// ABSORBS ALL CLICK EVENTS
+                child: AbsorbPointer(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[200],
+                    ),
+                    onPressed: () {
+                      print("AbsorbPointer");
+                    },
+                    child: null,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
         Container(width: 100, height: 100, color: Colors.blue[100]),
         Container(width: 100, height: 100, color: Colors.blue[100]),
       ],
