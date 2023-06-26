b7eff160326fab8c470b2bfcb60f7c529932524a
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-06-26 13:14:51 +0300

    Update Hero.dart

--- 002 Widget List/Hero.dart
+++ 002 Widget List/Hero.dart
@@ -28,12 +28,20 @@ class HeroExample extends StatelessWidget {
         ListTile(
           leading: const Hero(
             tag: "hero-rectangle-blue",
-            child: BoxWidget(size: Size(50.0, 50.0)),
+            child: BoxWidget(size: Size(50.0, 50.0), color: Colors.blue),
           ),
           onTap: () => _gotoDetailsPage(context),
           title:
               const Text('Tap on the icon to view hero animation transition.'),
         ),
+        const SizedBox(height: 20.0),
+        ListTile(
+            leading: const Hero(
+              tag: "hero-red",
+              child: BoxWidget(size: Size(50, 50), color: Colors.red),
+            ),
+            onTap: () => _gotoRedPage(context),
+            title: const Text("Second Hero Widget")),
       ],
     );
   }
@@ -45,7 +53,21 @@ class HeroExample extends StatelessWidget {
         body: const Center(
           child: Hero(
             tag: "hero-rectangle-blue",
-            child: BoxWidget(size: Size(200, 200)),
+            child: BoxWidget(size: Size(200, 200), color: Colors.blue),
+          ),
+        ),
+      ),
+    ));
+  }
+
+  void _gotoRedPage(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Center(child: Text("HeroDetails red"))),
+        body: const Center(
+          child: Hero(
+            tag: "her-red",
+            child: BoxWidget(size: Size(200, 200), color: Colors.red),
           ),
         ),
       ),
@@ -54,14 +76,15 @@ class HeroExample extends StatelessWidget {
 }
 
 class BoxWidget extends StatelessWidget {
-  const BoxWidget({super.key, required this.size});
+  const BoxWidget({super.key, required this.size, required this.color});
   final Size size;
+  final Color color;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: size.width,
       height: size.height,
-      color: Colors.blue,
+      color: color,
     );
   }
 }
