55138d615efb46b3ea9d23406c28c22eaa8f1088
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-07-19 16:37:16 +0300

    Update simpleCalculator.dart

--- 003 GetX/simpleCalculator.dart
+++ 003 GetX/simpleCalculator.dart
@@ -1,66 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-void main() => runApp(GetMaterialApp(
-  debugShowCheckedModeBanner: false,
-  home:const Home()));
-class Controller extends GetxController{
-var count = 0.obs;
-increment() => count++;
-decrement() => count--;
 
+void main() => runApp(
+    GetMaterialApp(debugShowCheckedModeBanner: false, home: const Home()));
+
+class Controller extends GetxController {
+  var count = 0.obs;
+  increment() => count++;
+  decrement() => count--;
 }
-class Home extends StatelessWidget{
+
+class Home extends StatelessWidget {
   const Home({super.key});
-@override
-Widget build(context){
-final Controller c = Get.put(Controller());
-return Scaffold(
- appBar:AppBar(title: Center(
-  child: Obx(() => Text("Clicks: ${c.count}")))),
-body: Center(
-child:Column(
-mainAxisAlignment: MainAxisAlignment.center,
-children: <Widget>[
-  Obx(()=>Text("Clicks: ${c.count}")),
-  const SizedBox(height: 20),
-  Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      ElevatedButton(
-        child: const Text("Add"),
-        onPressed: c.increment,
+  Widget build(Context) {
+    final Controller c = Get.put(Controller());
+    return Scaffold(
+      appBar: AppBar(
+          title: Center(
+        child: Obx(() => Text("Clicks: ${c.count}")),
+      )),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: const Icon(Icons.add),
+                onPressed: c.increment,
+              ),
+              const SizedBox(width: 30),
+              Obx(() => Text("Home Page Counts: ${c.count}")),
+              const SizedBox(width: 30),
+              ElevatedButton(
+                child: const Icon(Icons.remove),
+                onPressed: c.decrement,
+              ),
+            ],
+          ),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.navigation),
+        onPressed: () => Get.to(Other()),
       ),
-      const SizedBox(width: 20),
-      ElevatedButton(
-        child: const Text("Subs"),
-        onPressed: c.decrement,
-      ),
-      const SizedBox(width: 20),
-      ElevatedButton(
-        child: const Text("reset"),
-        onPressed:(){},
-      ),
-    ],
-  ),
-],
-),
+    );
+  }
+}
 
-),
-floatingActionButton: FloatingActionButton(
-child: Icon(Icons.route),
-onPressed: ()=> Get.to( Other()),
-),
-);
-}
-}
-class Other extends StatelessWidget{
- Other({super.key});
-final Controller c = Get.find();
-@override
-  Widget build(BuildContext context){
-     // Access the updated count variable
-     return Scaffold(
-      appBar: AppBar(title:const Center(child: Text("Second"))),
-      body: Center(child: Obx(()=>Text("Clicks: ${c.count}")),));
+class Other extends StatelessWidget {
+  final Controller c = Get.find();
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Center(
+        child: Obx(() => Text("Second: ${c.count}")),
+      )),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: const Text("Add"),
+                onPressed: c.increment,
+              ),
+              const SizedBox(width: 30),
+              Obx(() => Text("Other Page Counts: ${c.count}")),
+              const SizedBox(width: 30),
+              ElevatedButton(
+                child: const Text("Remove"),
+                onPressed: c.decrement,
+              ),
+            ],
+          ),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.home),
+        onPressed: () => Get.to(Home()),
+      ),
+    );
   }
 }
