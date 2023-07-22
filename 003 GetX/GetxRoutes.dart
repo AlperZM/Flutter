fab3028ce34d2e7c013be1980fdf42714e30b2f4
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-07-22 10:25:28 +0300

    Update GetxRoutes.dart

--- 003 GetX/GetxRoutes.dart
+++ 003 GetX/GetxRoutes.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-void main()=> runApp(
-  GetMaterialApp(
-debugShowCheckedModeBanner: false,
-home: const LogIn(),
-  ),
-);
-class LogIn extends StatelessWidget{
+
+void main() => runApp(
+      GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const LogIn(),
+      ),//GetMaterialApp
+    );
+
+class Controller extends GetxController {
+  // ---->> Login Page Controllers
+  var userName = "User".obs;
+  TextEditingController _controller = TextEditingController();
+  changeUserName(String val) {
+    userName.value = val;
+    val = "";
+    _controller.clear();
+  }
+  // ----> end of the login page controllers
+}
+
+class LogIn extends StatelessWidget {
   const LogIn({super.key});
   @override
-  Widget build(context){
+  Widget build(context) {
+    final Controller c = Get.put(Controller());
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("Route: LogIn")),
+        title: Center(child: Obx(() => Text("Route: ${c.userName}"))),
         actions: [
-            IconButton(
+          IconButton(
             tooltip: "Theme",
             onPressed: () {
               Get.changeTheme(
@@ -22,10 +37,50 @@ class LogIn extends StatelessWidget{
             },
             icon: const Icon(
               Icons.model_training_outlined,
-            ),
-          ),
+            ),//Icon
+          ),// IconButton
         ],
       ),
-    );
+      body: Center(
+        child: Card(
+          elevation: 32,
+          shadowColor: Theme.of(context).colorScheme.secondary,
+          child: Container(
+            width: 320,
+            height: 320,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1,
+                color: Theme.of(context).colorScheme.secondary,
+              ),// Border.all
+              borderRadius: BorderRadius.circular(5),
+            ),//BoxDecoration
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Please Log In",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextField(
+                    controller: c._controller,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter your username',
+                    ),// InputDecoration
+                    onSubmitted: c.changeUserName,
+                  ),// TextField
+                ),// Padding
+              ],
+            ),// Column
+          ),// Container
+        ),//Card
+      ),// Center
+    );// Scaffold
   }
 }
