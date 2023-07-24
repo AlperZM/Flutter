e00da7aa0fb3522b63c9efb67b0b955f6c546624
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-07-24 12:52:18 +0300

    Update controls.dart

--- 003 GetxRoteDemo/controls.dart
+++ 003 GetxRoteDemo/controls.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'home.dart';
+import 'controls.dart';
+import 'drawer.dart';
 
-class Controller extends GetxController {
-
-  // ---->> Login Page Controllers
-  var userName = "User".obs;
-  TextEditingController cont = TextEditingController();
-  changeUserName(String val) {
-    userName.value = val;
-    val = "";
-    cont.clear();
-    Get.toNamed("/home.dart");
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(context) {
+    final Controller c = Get.find();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Obx(() => Text("Home: ${c.userName}"))),
+        actions: [
+          IconButton(
+            tooltip: "Theme",
+            onPressed: () {
+              Get.changeTheme(
+                  Get.isDarkMode ? ThemeData.light() : ThemeData.dark());
+            },
+            icon: const Icon(
+              Icons.model_training_outlined,
+            ), //Icon
+          ), // IconButton
+        ],
+      ),
+      body: Center(child: Column()),
+      drawer: AppDrawer(),
+    );
   }
-  // ----> end of the login page controllers
 }
