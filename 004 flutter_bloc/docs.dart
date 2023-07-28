ff44f2ecb900721a7ef34cac9e3449b5bb9e1c4f
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-07-28 09:47:51 +0300

    Update docs.dart

--- 004 flutter_bloc/docs.dart
+++ 004 flutter_bloc/docs.dart
@@ -12,3 +12,58 @@ BlocBuilder<BlocA, BlocAState>(
     // return widget here based on BlocA's state
   }
 )
+
+  // buildWhen: compares two states and if return true creates new widget
+  BlocBuilder<BlocA, BlocAState>(
+  buildWhen: (previousState, state) {
+    // return true/false to determine whether or not
+    // to rebuild the widget with state
+  },
+  builder: (context, state) {
+    // return widget here based on BlocA's state
+  }
+)
+
+  //update or filter the state
+  BlocSelector<BlocA, BlocAState, SelectedState>(
+  selector: (state) {
+    // return selected state based on the provided state.
+  },
+  builder: (context, state) {
+    // return widget here based on the selected state.
+  },
+)
+
+// creates a bloc and deliver it to all children.
+  // like teme...
+  BlocProvider(
+  create: (BuildContext context) => BlocA(),
+  child: ChildA(),
+);
+BlocProvider.value(
+  value: BlocProvider.of<BlocA>(context),
+  child: ScreenA(),
+);
+// with extensions
+context.read<BlocA>();
+
+// without extensions
+BlocProvider.of<BlocA>(context)
+// for multi blocs
+  MultiBlocProvider(
+  providers: [
+    BlocProvider<BlocA>(
+      create: (BuildContext context) => BlocA(),
+    ),
+    BlocProvider<BlocB>(
+      create: (BuildContext context) => BlocB(),
+    ),
+    BlocProvider<BlocC>(
+      create: (BuildContext context) => BlocC(),
+    ),
+  ],
+  child: ChildA(),
+)
+
+
+  
