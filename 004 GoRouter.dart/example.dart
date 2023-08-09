0863a1367b1a184313f02d0d78c06a1d20353bf9
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-08-09 12:16:40 +0300

    Update example.dart

--- 004 GoRouter.dart/example.dart
+++ 004 GoRouter.dart/example.dart
@@ -1,122 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-void main() => runApp(GoRouterApp());
-final _router = GoRouter(
-initialLocation: "/",
-  routes: [
+
+void main() => runApp(const GoRouterApp());
+final GoRouter _router = GoRouter(
+  initialLocation: "/",
+  routes: <RouteBase>[
     GoRoute(
-    name: "home",
+      name: "home",
       path: "/",
-      builder: (context, state) => HomePage(),
+      builder: (context, state) {
+        return const HomePage();
+      },
     ),
     GoRoute(
-    name:"second",
+      name: "second",
       path: "/second",
-      builder: (context, state) => SecondPage(),
+      builder: (context, state) {
+        return const SecondPage();
+      },
     ),
     GoRoute(
-    name: "third",
+      name: "third",
       path: "/third",
-      builder: (context, state) => ThirdPage(),
+      builder: (context, state) {
+        return const ThirdPage();
+      },
     ),
   ],
 );
 
-class GoRouterApp extends StatelessWidget{
+class GoRouterApp extends StatelessWidget {
+  const GoRouterApp({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp.router(
-    debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       routerConfig: _router,
     );
   }
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-    appBar:AppBar(title:const Center(child:Text("GoRouter-Home"))),
-      body: Center(child:Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(title: const Center(child: Text("GoRouter-Home"))),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Home Page",
-        style: Theme.of(context).textTheme.titleLarge,),
-          const SizedBox(height: 10),
-          ElevatedButton(
-          child: const Text("Home"),
-            onPressed:(){context.go("/");},
+          Text(
+            "Home Page",
+            style: Theme.of(context).textTheme.titleLarge,
           ),
           const SizedBox(height: 10),
           ElevatedButton(
-          child: const Text("Second"),
-            onPressed:(){context.go("/second");},
+            child: const Text("Home"),
+            onPressed: () {
+              context.push("/");
+            },
           ),
           const SizedBox(height: 10),
           ElevatedButton(
-          child: const Text("Third"),
-            onPressed:(){context.go("/third");},
-          ),
-        ],
-      )),
-    );
-  }
-}
-class SecondPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-    appBar:AppBar(title:const Center(child:Text("GoRouter-Second"))),
-      body: Center(child:Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Second Page",
-        style: Theme.of(context).textTheme.titleLarge,),
-          const SizedBox(height: 10),
-          ElevatedButton(
-          child: const Text("Home"),
-            onPressed:(){context.go("/");},
+            child: const Text("Second"),
+            onPressed: () {
+              context.push("/second");
+            },
           ),
           const SizedBox(height: 10),
           ElevatedButton(
-          child: const Text("Second"),
-            onPressed:(){context.go("/second");},
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-          child: const Text("Third"),
-            onPressed:(){context.go("/third");},
-          ),
-        ],
-      )),
-    );
-  }
-}
-class ThirdPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-    appBar:AppBar(title:const Center(child:Text("GoRouter-Third"))),
-      body: Center(child:Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Third Page",
-        style: Theme.of(context).textTheme.titleLarge,
-              ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-          child: const Text("Home"),
-            onPressed:(){context.go("/");},
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-          child: const Text("Second"),
-            onPressed:(){context.go("/second");},
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-          child: const Text("Third"),
-            onPressed:(){context.go("/third");},
+            child: const Text("Third"),
+            onPressed: () {
+              context.push("/third");
+            },
           ),
         ],
       )),
@@ -124,6 +81,84 @@ class ThirdPage extends StatelessWidget{
   }
 }
 
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text("GoRouter-Second"))),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Second Page",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            child: const Text("Home"),
+            onPressed: () {
+              context.push("/");
+            },
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            child: const Text("Second"),
+            onPressed: () {
+              context.push("/second");
+            },
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            child: const Text("Third"),
+            onPressed: () {
+              context.push("/third");
+            },
+          ),
+        ],
+      )),
+    );
+  }
+}
 
-
-
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text("GoRouter-Third"))),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Third Page",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            child: const Text("Home"),
+            onPressed: () {
+              context.push("/");
+            },
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            child: const Text("Second"),
+            onPressed: () {
+              context.push("/second");
+            },
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            child: const Text("Third"),
+            onPressed: () {
+              context.push("/third");
+            },
+          ),
+        ],
+      )),
+    );
+  }
+}
