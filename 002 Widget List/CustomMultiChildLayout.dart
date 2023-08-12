97248eedf89dbcc110c55313775ee158197c9aba
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-08-12 10:34:50 +0300

    Update CustomMultiChildLayout.dart

--- 002 Widget List/CustomMultiChildLayout.dart
+++ 002 Widget List/CustomMultiChildLayout.dart
@@ -1,102 +1,155 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const CustomMultiChildApp());
+void main() {
+  runApp(MyApp());
+}
 
-class CustomMultiChildApp extends StatelessWidget {
-  const CustomMultiChildApp({super.key});
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Directionality(
-        textDirection: TextDirection.ltr,
-        child: Scaffold(
-          appBar: AppBar(
-              title: const Text("CustomMultiChildLayout",
-                  textAlign: TextAlign.center)),
-          body: const ExampleWidget(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("CustomMultiChildLayout")),
+        body: Center(
+          child: CustomMultiChildExampleScreen(),
         ),
       ),
     );
   }
 }
 
-class ExampleWidget extends StatelessWidget {
-  const ExampleWidget({super.key});
-  static const Map<String, Color> _colors = <String, Color>{
-    "Red": Colors.red,
-    "Teal": Colors.teal,
-    "Amber": Colors.amber,
-    "Cyan": Colors.cyan,
-  };
+class CustomMultiChildExampleScreen extends StatefulWidget {
+  @override
+  State<CustomMultiChildExampleScreen> createState() =>
+      _CustomMultiChildExampleScreenState();
+}
+
+class _CustomMultiChildExampleScreenState
+    extends State<CustomMultiChildExampleScreen> {
+  double width = 300.0;
+  double height = 150.0;
+  double maxHeight = 50.0;
+  double maxWidth = 50.0;
+
   @override
   Widget build(BuildContext context) {
-    return CustomMultiChildLayout(
-      delegate: _CascadeLayoutDelegate(
-        colors: _colors,
-        overlap: 35,
-        textDirection: Directionality.of(context),
-      ),
-      children: <Widget>[
-        for (MapEntry<String, Color> entry in _colors.entries)
-          LayoutId(
-            id: entry.key,
-            child: Container(
-              color: entry.value,
-              width: 100,
-              height: 100,
-              alignment: Alignment.center,
-              child: Text(entry.key),
+    return Center(
+      child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: height,
+            width: width,
+            color: Colors.blue,
+            child: CustomMultiChildLayout(
+              delegate: _CustomMultiChildLayoutDelegate(
+                  maxHeight: maxHeight, maxWidth: maxWidth),
+              children: <Widget>[
+                LayoutId(
+                  id: 1,
+                  child: Container(
+                    height: 75.0,
+                    width: 75.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.red,
+                    ),
+                    child: const Center(child: Text('1')),
+                  ),
+                ),
+                LayoutId(
+                  id: 2,
+                  child: Container(
+                    height: 75.0,
+                    width: 75.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.green,
+                    ),
+                    child: const Center(child: Text('2')),
+                  ),
+                ),
+                LayoutId(
+                  id: 3,
+                  child: Container(
+                    height: 75.0,
+                    width: 75.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.cyan,
+                    ),
+                    child: const Center(child: Text('3')),
+                  ),
+                ),
+                LayoutId(
+                  id: 4,
+                  child: Container(
+                    height: 75.0,
+                    width: 75.0,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.orange),
+                    child: const Center(child: Text('4')),
+                  ),
+                ),
+                LayoutId(
+                  id: 5,
+                  child: Container(
+                    height: 75.0,
+                    width: 75.0,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.purpleAccent),
+                    child: const Center(child: Text('5')),
+                  ),
+                ),
+              ],
             ),
-          ),
-      ],
+          )),
     );
   }
 }
 
-class _CascadeLayoutDelegate extends MultiChildLayoutDelegate {
-  _CascadeLayoutDelegate({
-    required this.colors,
-    required this.overlap,
-    required this.textDirection,
-  });
+class _CustomMultiChildLayoutDelegate extends MultiChildLayoutDelegate {
+  _CustomMultiChildLayoutDelegate({this.maxHeight, this.maxWidth});
+  final maxHeight;
+  final maxWidth;
 
-  final Map<String, Color> colors;
-  final double overlap;
-  final TextDirection textDirection;
-
-  // Perform layout will be called when re-layout is needed.
   @override
   void performLayout(Size size) {
-    final double columnWidth = size.width / colors.length;
-    Offset childPosition = Offset.zero;
-    switch (textDirection) {
-      case TextDirection.rtl:
-        childPosition += Offset(size.width, 0);
-      case TextDirection.ltr:
-        break;
+    if (hasChild(1)) {
+      layoutChild(1, BoxConstraints(maxWidth: maxWidth, maxHeight: maxHeight));
+      double dx = ((size.width / 2) - (maxWidth / 2));
+      double dy = ((size.height / 2) - (maxHeight / 2));
+      positionChild(1, Offset(dx, dy));
     }
-    for (final String color in colors.keys) {
-      final Size currentSize = layoutChild(
-        color,
-        BoxConstraints(maxHeight: size.height, maxWidth: columnWidth),
-      );
-      switch (textDirection) {
-        case TextDirection.rtl:
-          positionChild(color, childPosition - Offset(currentSize.width, 0));
-          childPosition +=
-              Offset(-currentSize.width, currentSize.height - overlap);
-        case TextDirection.ltr:
-          positionChild(color, childPosition);
-          childPosition +=
-              Offset(currentSize.width, currentSize.height - overlap);
-      }
+    if (hasChild(2)) {
+      layoutChild(2, BoxConstraints(maxWidth: maxWidth, maxHeight: maxHeight));
+      double dx = -(maxWidth / 2);
+      double dy = -((maxHeight / 2));
+      positionChild(2, Offset(dx, dy));
+    }
+    if (hasChild(3)) {
+      layoutChild(3, BoxConstraints(maxWidth: maxWidth, maxHeight: maxHeight));
+      double dx = ((size.width) - (maxWidth / 2));
+      double dy = -((maxHeight / 2));
+      positionChild(3, Offset(dx, dy));
+    }
+    if (hasChild(4)) {
+      layoutChild(4, BoxConstraints(maxWidth: maxWidth, maxHeight: maxHeight));
+      double dx = ((size.width) - (maxWidth / 2));
+      double dy = ((maxHeight * 2) + (maxHeight / 2));
+      positionChild(4, Offset(dx, dy));
+    }
+    if (hasChild(5)) {
+      layoutChild(5, BoxConstraints(maxWidth: maxWidth, maxHeight: maxHeight));
+      double dx = -(maxWidth / 2);
+      double dy = ((maxHeight * 2) + (maxHeight / 2));
+      positionChild(5, Offset(dx, dy));
     }
   }
 
   @override
-  bool shouldRelayout(_CascadeLayoutDelegate oldDelegate) {
-    return oldDelegate.textDirection != textDirection ||
-        oldDelegate.overlap != overlap;
+  bool shouldRelayout(_CustomMultiChildLayoutDelegate oldDelegate) {
+    return (maxHeight != oldDelegate.maxHeight ||
+        maxWidth != oldDelegate.maxWidth);
   }
 }
