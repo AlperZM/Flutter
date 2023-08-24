cc87648b0880ea5aaa70eb5050a5b46b7e15c2ec
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-08-24 16:04:42 +0300

    Create LoginProject.dart

---
+++ Cookbook/LoginProject.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+void main()=> runApp(const SignUpApp());
+
+class SignUpApp extends StatelessWidget{
+  const SignUpApp();
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      routes: {
+        "/": (context) => const SignUpScreen(),
+      },
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget{
+  const SignUpScreen();
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+    backgroundColor: Colors.grey[200],
+      appBar:AppBar(title:const Text("Login")),
+      body:const Center(child:SizedBox(width:400,child:Card(
+      child:SignUpForm(
+      
+      )))),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget{
+  const SignUpForm();
+
+   @override
+   State<SignUpForm> createState() => _SignUpFormState();
+ }
+class _SignUpFormState extends State<SignUpForm> {
+  final _firstNameTextController = TextEditingController();
+  final _lastNameTextController = TextEditingController();
+  final _userNameTextController = TextEditingController();
+  double _formProgress = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Form(child:Column(
+    mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        LinearProgressIndicator(value: _formProgress),
+        Text("SignUp", style:Theme.of(context).textTheme.headlineMedium),
+        Container(
+        padding: const EdgeInsets.all(8),
+          child: TextFormField(
+          controller: _firstNameTextController,
+            decoration: const InputDecoration(hintText: 'First name'),
+          ),),
+        Container(
+        padding: const EdgeInsets.all(8),
+          child: TextFormField(
+          controller: _lastNameTextController,
+            decoration: const InputDecoration(hintText: 'Last name'),
+          ),),
+        Container(
+        padding: const EdgeInsets.all(8),
+          child: TextFormField(
+          controller: _userNameTextController,
+            decoration: const InputDecoration(hintText: 'User name'),
+          ),),
+        TextButton(
+        style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.resolveWith(
+        (Set<MaterialState> states){
+          return states.contains(MaterialState.disabled)
+            ? null : Colors.white;}),
+          backgroundColor:MaterialStateProperty.resolveWith(
+          (Set<MaterialState> states) {
+                 return states.contains(MaterialState.disabled)
+                     ? null
+                     : Colors.blue;}),
+        ),
+                     onPressed:(){
+                       Navigator.of(context).pushNamed('/welcome');},
+             child: const Text('Sign up')
+        ),
+      ],),);
+  }
+}
+
+
+class WellcomePage extends StatelessWidget{
+  const WellcomePage({super.key});
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+    appBar:AppBar(title:const Text("Wellcome: user")),
+      body: Center(child:Text("Wellcome: user"
+     , style: Theme.of(context).textTheme.displayLarge)),
+    );
+  }
+}
