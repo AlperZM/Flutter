b615259b0df9cd3a20d04f7d4b9523da0dae9b36
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-08-31 14:05:55 +0300

    Update ScrollApp.dart

--- Scrolling/ScrollApp.dart
+++ Scrolling/ScrollApp.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp( const MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,19 +9,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-      appBar:AppBar(title:const Text("Simple Scroll App")),
-        body:const Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+        appBar: AppBar(title: const Text("Simple Scroll App")),
+        body: const Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             SideMenu(),
             SizedBox(width: 10.0),
             Center(
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text("Use navigation menu for see examples"),
-              ],
-            ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Use navigation menu for see examples"),
+                ],
+              ),
             ),
           ],
         ),
@@ -35,47 +35,99 @@ class SingleChildScrollViewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title:const Text("SingleChildScrollView")),
+      appBar: AppBar(title: const Text("SingleChildScrollView")),
       body: Row(
-        mainAxisAlignment:MainAxisAlignment.start,
-      children:<Widget>[
-       const SideMenu(),
-       const SizedBox(width: 10.0),
-              Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Simple Scrollable Widget",
-                style: Theme.of(context).textTheme.titleLarge),
-            const SizedBox(height: 20),
-            Text("SingleChildScrollView",
-                style: Theme.of(context).textTheme.titleMedium),
-            Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(border: Border.all()),
-              child:const SingleChildScrollView(
-                  child:Column(
-                  children: <Widget>[
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          const SideMenu(),
+          const SizedBox(width: 10.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Simple Scrollable Widget",
+                  style: Theme.of(context).textTheme.titleLarge),
+              const SizedBox(height: 20),
+              Text("SingleChildScrollView",
+                  style: Theme.of(context).textTheme.titleMedium),
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(border: Border.all()),
+                child: const SingleChildScrollView(
+                  child: Column(children: <Widget>[
                     ListTile(title: Text("List Tile")),
-                    SizedBox(height:20.0),
+                    SizedBox(height: 20.0),
                     ListTile(title: Text("List Tile")),
-                    SizedBox(height:20.0),
+                    SizedBox(height: 20.0),
                     ListTile(title: Text("List Tile")),
-                    SizedBox(height:20.0),
+                    SizedBox(height: 20.0),
                     ListTile(title: Text("List Tile")),
-                    SizedBox(height:20.0),
-                    ListTile(title: Text("List Tile")),
-                    SizedBox(height:20.0),
-                  ]
+                    SizedBox(height: 20.0),
+                  ]),
                 ),
               ),
-              ),
-
-          ],
-    ),
-      ],
+            ],
+          ),
+        ],
       ),
+    );
+  }
+}
 
+class CustomScroll extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("CustomScrollView")),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 200,
+            height: 100,
+            decoration:
+                BoxDecoration(color: Colors.tealAccent, border: Border.all()),
+            child: CustomScrollView(
+              scrollDirection: Axis.vertical,
+              slivers: <Widget>[
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return ListTile(
+                        title: Text("List: $index"),
+                      );
+                    },
+                    childCount: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 20.0),
+          Container(
+            width: 200,
+            height: 100,
+            decoration: BoxDecoration(border: Border.all()),
+            child: CustomScrollView(
+              scrollDirection: Axis.horizontal,
+              slivers: <Widget>[
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      return Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.blue,
+                          margin: const EdgeInsets.all(10));
+                    },
+                    childCount: 20,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -102,11 +154,22 @@ class SideMenu extends StatelessWidget {
                 context,
                 MaterialPageRoute(builder: (context) {
                   return const SingleChildScrollViewApp();
-                  }),
+                }),
               );
             },
           ),
           const SizedBox(height: 10),
+          TextButton(
+            child: const Text("Custom"),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return CustomScroll();
+                }),
+              );
+            },
+          ),
         ],
       ),
     );
