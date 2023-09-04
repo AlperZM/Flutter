f33901266d1778cc60df2ab6191a2b975adec4af
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-09-04 13:01:59 +0300

    Create FittedBox.dart

---
+++ Adaptive and Responsive/FittedBox.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const FittedBoxApp());
+
+class FittedBoxApp extends StatelessWidget {
+  const FittedBoxApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('FittedBox Sample')),
+        body: const Center(
+          child: FittedBoxExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class FittedBoxExample extends StatelessWidget {
+  const FittedBoxExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      width: 300,
+      color: Colors.blue,
+      child: const FittedBox(
+        fit: BoxFit.fill,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
