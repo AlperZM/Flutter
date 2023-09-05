e1b818dfb9d9df07c96aa621b049d2128a064b39
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-09-05 14:18:50 +0300

    Update AllinOneDemos.dart

--- 004 GoRouter.dart/AllinOneDemos.dart
+++ 004 GoRouter.dart/AllinOneDemos.dart
@@ -9,8 +9,14 @@ final _router = GoRouter(
       builder: (context, state) => const HomePage(),
     ),
     GoRoute(
-    path:"/align",
-      builder:(context, state)=> const AlignPage(),
+      path: "/align",
+      builder: (context, state) => const AlignPage(),
+      routes: [
+        GoRoute(
+          path: "fractionalAlign",
+          builder: (context, state) => const FractionalAlignPage(),
+        ),
+      ],
     ),
   ],
 );
@@ -34,50 +40,74 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Center(child: Text("AdaptiveApp"))),
-      body:const Center(child: Text("use drawer to navigate")),
+      body: const Center(child: Text("use drawer to navigate")),
       drawer: Drawer(
-      child: ListView(
-      padding: EdgeInsets.zero,
-        children: <Widget>[
-         const DrawerHeader(
-          decoration: BoxDecoration(color: Colors.teal),
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Chip(
-              label: Icon(Icons.menu),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.teal),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Chip(
+                    label: Icon(Icons.menu),
+                  ),
+                  Text("Menu",
+                      style: TextStyle(fontSize: 20, color: Colors.white)),
+                  Text("Use List Tiles Belov"),
+                ],
               ),
-              Text("Menu",style:TextStyle(fontSize: 20, color: Colors.white)),
-              Text("Use List Tiles Belov"),
-              
-            ],
             ),
-          ),
-          ListTile(
-          leading: const Icon(Icons.align_horizontal_center),
-            title: const Text("Go Align Widgets"),
-            onTap:()=> context.go("/align"),
-          ),
-        ],
-      ),
+            ListTile(
+              leading: const Icon(Icons.align_horizontal_center),
+              title: const Text("Go Align Widgets"),
+              onTap: () => context.go("/align"),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
 class AlignPage extends StatelessWidget {
   const AlignPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-             leading: IconButton(
-      icon:const Icon(Icons.arrow_back),
-        onPressed:()=> context.go("/"),
-      ),
-        title: const Center(child: Text("SecondPage"))),
-      body: Center(child:ElevatedButton(
-      child: const Text("Align Demos Page"),
-        onPressed: (){context.go("/");}
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () => context.go("/"),
+          ),
+          title: const Center(child: Text("SecondPage"))),
+      body: Center(
+          child: ElevatedButton(
+              child: const Text("FractionalAlign Demos Page"),
+              onPressed: () {
+                context.go("/fractionalAlign");
+              })),
+    );
+  }
+}
+
+class FractionalAlignPage extends StatelessWidget {
+  const FractionalAlignPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () => context.go("/align"),
+          ),
+          title: const Center(child: Text("Align"))),
+      body: Center(
+          child: Container(
+        width: 200,
+        height: 200,
+        color: Colors.amber,
       )),
     );
   }
